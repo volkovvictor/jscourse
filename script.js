4fa22d339c5a75eb27cb92e6349a73a6a575a289
Victor Volkov <vityan2504@gmail.com>
2023-11-03 16:19:42 +0300

    Добавил значения переменным, вывел информацию из переменных в консоль

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 4000;
+let rollback = 20;
+let fullPrice = 100000;
+let adaptive = true;
 
-console.log('Какой-то текст');
-alert("Тоже какой-то текст");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100));
