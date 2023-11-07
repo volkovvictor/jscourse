921f9ce77da853c3caeb9bd09c9aa7a6d7b94424
Victor Volkov <vityan2504@gmail.com>
2023-11-07 17:26:31 +0300

    Вывел итоговую стоимость в консоль на основе полученных данных

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let title = "Название проекта";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 4000;
-let rollback = 20;
-let fullPrice = 100000;
-let adaptive = true;
+const title = prompt("Как называется ваш проект?", "Название");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+const screenPrice = +prompt("Сколько будет стоить данная работа?", 20000);
+const rollback = 20;
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+
+const service1 = prompt("Какой дополнительный тип услуги нужен?", "Анимация");
+const servicePrice1 = +prompt("Сколько это будет стоить?", 5000);
+const service2 = prompt("Какой дополнительный тип услуги нужен?", "Метрика");
+const servicePrice2 = +prompt("Сколько это будет стоить?", 3000);
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+
+console.log("Итоговая стоимость " + servicePercentPrice);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
